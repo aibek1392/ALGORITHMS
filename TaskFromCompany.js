def1d19fbde411c79787eb289f597e16b7877fb3
aibek <ozhorov@gmail.com>
2020-03-29 18:51:56 -0400

    1 more algo

--- TaskFromCompany.js
+++ TaskFromCompany.js
@@ -115,3 +115,20 @@ function validBraces(braces){
 
   return stack.length === 0; // any unclosed braces left?
 }
+
+//
+// Given: an array containing hashes of names
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+function list(names){
+  return names.reduce(function(prev, current, index, array){
+    if (index === 0){
+      return current.name;
+    }
+    else if (index === array.length - 1){
+      return prev + ' & ' + current.name;
+    } 
+    else {
+      return prev + ', ' + current.name;
+    }
+  }, '');
+ }
