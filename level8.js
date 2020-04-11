85f935b4ac550e5a8b05b9517222ed48aa7ccaef
aibek <ozhorov@gmail.com>
2020-04-11 06:25:11 -0400

    more algos

--- level8.js
+++ level8.js
@@ -32,10 +32,37 @@ function find_average(array) {
     result += array.reduce(reducer)
     return result / array.length;
 }
-    // Test.assertEquals(abbrevName("Sam Harris"), "S.H");
+// Test.assertEquals(abbrevName("Sam Harris"), "S.H");
 
 function abbrevName(name) {
     let result = ''
     result += name.split(' ').map(n => n.charAt(0).toUpperCase()).join('.')
 }
 
+
+// Test.assertEquals(bonusTime(25000, true), '£250000');
+// Test.assertEquals(bonusTime(10000, false), '£10000');
+function bonusTime(salary, bonus) {
+    let result = '';
+    return result += bonus ? "£" + salary * 10 : "£" + salary
+}
+
+//   how to change letter in a string
+const DNAtoRNA = (dna) => dna.split('T').join('U')
+// sum of numbers  in an array 
+function sum(numbers) {
+    return numbers.reduce((a, b) => a + b, 0);
+}
+
+//   if else statement
+function areYouPlayingBanjo(name) {
+    // Implement me
+    return name[0] === "R" || name[0] === 'r' ? name + " plays banjo" : name + " does not play banjo"
+}
+
+//sort.slice.reduce
+function sumArray(array) {
+    console.log(array)
+    if (array == null || array.length == 0 || array.length == 1) return 0
+    return array.sort((a, b) => a - b).slice(1, -1).reduce((a, b) => a + b, 0)
+}
