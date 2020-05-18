9812debf279bfa96b170f21eb339377340a7b262
aibek <ozhorov@gmail.com>
2020-05-17 21:45:41 -0400

    medium lvl

--- Leetcode.js
+++ Leetcode.js
@@ -92,3 +92,35 @@ console.log(nums);
 
 let arr = [1, 1, 2]
 removeDuplicates(arr);
+
+
+//algorithms Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+// Input: "babad"
+// Output: "bab"
+// Note: "aba" is also a valid answer
+var longestPalindrome = function(s) {
+  let palindromes = [];
+
+  if(s.length <= 1) {
+      return s;
+  }
+
+  for (var i = 0; i < s.length; i += 1) {
+    j = 0;
+    k = 0;
+    while(s[i + j] === s[i - j] && s[i + j] && s[i - j]) {
+      j++;
+    };
+    palindromes.push(s.slice(i-(j-1), i+j)) //even palindromes
+    j = 0;
+
+    while(s[(i + 1) + k] === s[i - k] && s[(i + 1) + k] && s[i - k]){
+      k++;
+    }
+    palindromes.push(s.slice(i-(k-1),(i+1)+k)) //odd palindromes
+    k = 0;
+  }
+  return palindromes.sort((a, b) => {
+      return b.length - a.length
+  })[0]; 
+};
