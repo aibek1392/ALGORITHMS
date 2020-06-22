665585b9fc484013a3f9357118a5698db94a67d5
aibek <ozhorov@gmail.com>
2020-06-21 22:19:02 -0400

    codewar codechallenge

--- Codewars.js
+++ Codewars.js
@@ -2,18 +2,18 @@
 //  the string into pairs of two characters. 
 // If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
-function solution(str){
-    let splittedStr = str.split('')
-    let arrayOfOddNumbers = []
-    for(let i = 0; i < splittedStr.length; i+=2){
-     if(splittedStr[i+1] ===undefined){
-       splittedStr[i+1] = "_"
-       }
-     let stringA = splittedStr[i]+ splittedStr[i+1]
-       arrayOfOddNumbers.push(stringA)
-      }
-     return arrayOfOddNumbers 
+function solution(str) {
+  let splittedStr = str.split('')
+  let arrayOfOddNumbers = []
+  for (let i = 0; i < splittedStr.length; i += 2) {
+    if (splittedStr[i + 1] === undefined) {
+      splittedStr[i + 1] = "_"
+    }
+    let stringA = splittedStr[i] + splittedStr[i + 1]
+    arrayOfOddNumbers.push(stringA)
   }
+  return arrayOfOddNumbers
+}
 
 //   console.log()--> 'ab', 'cd', 'ef', 'g_' 
 
@@ -41,9 +41,9 @@ function regexContainsAll(str) {
 function findNumber(array) {
   const arr = array.reduce(
     (acc, value, index) => [acc[0] + value, acc[1] + index],
-    [0, array.length*2+1]
+    [0, array.length * 2 + 1]
   );
-  
+
   return arr[1] - arr[0]
 }
 
@@ -55,24 +55,45 @@ function howManyPalindromes(s) {
   console.log(s)
   counts = s.length
   if (s[0].repeat(s.length) == s)
-      return s.length * (s.length + 1) / 2
+    return s.length * (s.length + 1) / 2
   for (i = 2; i <= s.length; i++)
-      for (j = 0; j <= s.length - i; j++) {
-          counts++
-          for (k = 0; k < i / 2; k++)
-              if (s[j + k] != s[j + i - k - 1]) {
-                  counts--
-                  break
-              }
-      }
+    for (j = 0; j <= s.length - i; j++) {
+      counts++
+      for (k = 0; k < i / 2; k++)
+        if (s[j + k] != s[j + i - k - 1]) {
+          counts--
+          break
+        }
+    }
   return counts
 }
 
 // Your task is to check wheter a given integer is a perfect power. If it is a perfect power, return a pair m and k with mk = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
-var isPP = function(n){
+var isPP = function (n) {
   for (var m = 2; m <= Math.floor(Math.sqrt(n)); ++m) {
     var k = Math.round(Math.log(n) / Math.log(m))
     if (Math.pow(m, k) == n) return [m, k];
   }
   return null;
 }
+
+// Convert A Hex String To RGB
+function hexStringToRGB(s) {
+  return {
+    "r": parseInt(s.slice(1, 3), 16),
+    "g": parseInt(s.slice(3, 5), 16),
+    "b": parseInt(s.slice(5, 7), 16)
+  };
+}
+
+const GetFormat = elList => {
+  return new Proxy((...a) => {
+    return elList.reduce((html, el) => `<${el}>${html}</${el}>`, a.join``);
+  }, {
+    get: (_, el) => {
+      return GetFormat([el, ...elList]);
+    }
+  });
+};
+
+const Format = GetFormat([]);
