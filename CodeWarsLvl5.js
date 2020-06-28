582ce65dbc46c17926d3677b2f877411a6150ec3
aibek <ozhorov@gmail.com>
2020-06-28 16:59:23 -0400

    thursday algo

--- CodeWarsLvl5.js
+++ CodeWarsLvl5.js
@@ -10,3 +10,31 @@ function longestMotif(seq) {
     let ml = Math.max(...li.map(x => x.length))
     return [...new Set(li.filter(x => x.length == ml))]
 }
+
+
+//Ulam Sequences(performance edition)
+function ulamSequence(n1, n2, q) {
+    const tome = [n1, n2];
+    const codex = new Set([n1 + n2]);
+    const reject = new Set();
+    let cv = n1 + n2,
+        c = 2;
+    while (c < q) {
+        if (codex.has(cv)) {
+            for (let n of tome) {
+                const cvn = cv + n;
+                if (codex.has(cvn)) {
+                    codex.delete(cvn);
+                    reject.add(cvn);
+                }
+                else if (!reject.has(cvn)) {
+                    codex.add(cvn);
+                }
+            }
+            tome.push(cv);
+            c++;
+        }
+        cv++;
+    }
+    return tome;
+}
