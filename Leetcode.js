5c2d3a4c5bdc356750c7ce9ed3b4054e1b655df5
aibek <ozhorov@gmail.com>
2020-06-28 16:55:16 -0400

    zigzag conversation

--- Leetcode.js
+++ Leetcode.js
@@ -290,3 +290,21 @@ var addTwoNumbers = function (l1, l2) {
   }
   return head;
 };
+
+
+///ZIg ZAG Conversation
+var length = s.length;
+if (numRows == 1) return s;
+var cycleLen = numRows * 2 - 2;
+var aZig = [];
+for (var i = 0; i < numRows; i++) {
+  for (var j = 0; j + i < length; j = j + cycleLen) {
+    aZig.push(s.charAt(j + i));
+    if (i != 0 && i != numRows - 1 && j + cycleLen - i < length) {
+      aZig.push(s.charAt(j + cycleLen - i));
+    }
+  }
+}
+
+return aZig.join("");
+};
