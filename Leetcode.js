74b0bf8e64efe31d11823fefe5bf6efdfada7dc6
aibek <ozhorov@gmail.com>
2020-06-28 16:52:48 -0400

    Medium lvl Leetcode challenge

--- Leetcode.js
+++ Leetcode.js
@@ -246,3 +246,47 @@ var isAnagram = function (s, t) {
   }
   return true;
 };
+
+
+
+//Add Two numbers
+var addTwoNumbers = function (l1, l2) {
+  let head, c = head, one = l1, two = l2, carry = 0;
+
+
+  while (one || two) {
+    let x = 0, y = 0, sum = carry, next, tmpNode;
+    if (one.val) {
+      x = one.val;
+    }
+    if (two.val) {
+      y = two.val;
+    }
+    sum += x + y;
+    carry = Math.floor(sum / 10);
+    next = sum % 10;
+    tmpNode = new ListNode(next);
+    if (head == null) {
+      head = new ListNode(next);
+      c = head;
+    } else {
+      c.next = tmpNode;
+      c = c.next;
+    }
+    if (one.next != null) {
+      one = one.next;
+    } else {
+      one = false
+    }
+    if (two.next != null) {
+      two = two.next;
+    } else {
+      two = false
+    }
+  }
+  if (carry > 0) {
+    let tmpNode = new ListNode(carry);
+    c.next = tmpNode;
+  }
+  return head;
+};
