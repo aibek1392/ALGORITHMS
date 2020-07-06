72b4b109558549abb64a489c83515be3d4404448
aibek <ozhorov@gmail.com>
2020-07-05 22:36:19 -0400

    lvl6

--- CodewarsLvl6.js
+++ CodewarsLvl6.js
@@ -159,3 +159,35 @@ function decode(r) {
     res += a[(a.indexOf(c) * n) % 26];
   return res;
 }
+
+
+//disease Spread 
+function epidemic(tm, n, s0, i0, b, a) {
+  const dt = tm / n
+  const iArr = new Array(n).fill()
+  const sArr = new Array(n).fill()
+  const rArr = new Array(n).fill()
+  
+  iArr[0] = i0
+  sArr[0] = s0
+  rArr[0] = 0
+  
+  iArr.slice(1).forEach((_, i) => {    
+    sArr[i + 1] = sArr[i] - dt * b * sArr[i] * iArr[i]
+    iArr[i + 1] = iArr[i] + dt * (b * sArr[i] * iArr[i] - a * iArr[i])
+    rArr[i + 1] = rArr[i] + dt * iArr[i] * a
+  })
+  
+  return Math.ceil(Math.max.apply(null, iArr))
+}
+
+//braking well
+function dist(v, mu) {
+  v = v * 10 / 36;
+  return v + (v * v) / (2 * mu * 9.81);
+}
+
+function speed(d, mu) {
+  let x = 2 * mu * 9.81;
+  return (-x + Math.sqrt(x * x + 4 * d * x)) * 36 / 20;
+}
