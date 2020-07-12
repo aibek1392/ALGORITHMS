d09e51b15cf90c15d3884d8f21e72e81fec89505
aibek <ozhorov@gmail.com>
2020-07-12 17:54:49 -0400

    group Anagram solution

--- LeetcodeMed.js
+++ LeetcodeMed.js
@@ -80,3 +80,22 @@ var convert = function (s, numRows) {
 };
 
 
+
+//Group Anagram solution
+var groupAnagrams = function (strs) {
+    let map = new Map();
+    for (let str of strs) {
+        const s = uniform(str);
+        let l = map.get(s) || [];
+        l.push(str);
+        map.set(s, l);
+    }
+    return Array.from(map.values());
+};
+
+var uniform = function (str) {
+    return str
+        .split("")
+        .sort()
+        .join("");
+};
