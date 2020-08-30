aa994200021972b1840a9e8eb708cb80bf12204f
aibek <ozhorov@gmail.com>
2020-08-30 13:46:31 -0400

    snake algo

--- Codewars.js
+++ Codewars.js
@@ -244,3 +244,20 @@ const solve = (m) => {
   }
   return x;
 };
+
+//snake
+const distribution=function(size) {
+  let side = Math.ceil((2+2*size)**.5-1);
+  side += (side+1)*(side+1>>1) < size
+  let M = Array(side).fill(0);
+  M = M.map(_=>M.slice());
+  
+  let dirs=[[1,0,side-1], [0,1,2], [-1,0,side-1], [0,1,2]];
+  let x=0, y=0, i=0, n=side-1, dx=1, dy=0;
+  while(size-->0){
+    M[x][y]=1;
+    if(!n) [dx,dy,n] = dirs[i=(++i)%4];
+    x+=dx; y+=dy; n--;
+  }
+  return M;
+}
